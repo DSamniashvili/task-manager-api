ac43a1229a7aebe94ddd105f4876a6ac3b7364ff
dea <dea@newrow.com>
2021-01-02 01:18:45 +0400

    added ability to upload image with multer

--- src/routes/user-router.js
+++ src/routes/user-router.js
@@ -2,6 +2,70 @@ const express = require('express');
 const User = require('../models/user-model');
 const userRouter = new express.Router();
 const auth = require('../middleware/auth');
+const multer = require('multer');
+
+const upload = multer({
+    limits: {
+        fileSize: 1000000
+    },
+    fileFilter(req, file, cb) {
+        if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+            return cb(new Error('Please upload only image files'))
+        }
+        cb(undefined, true)
+    }
+});
+
+
+userRouter.post('/users/profile/upload', auth, upload.single('avatar'), async (req, res) => {
+    const imageBuffer = req.file.buffer;
+    const user = req.user;
+
+    user.avatar = imageBuffer;
+    await user.save();
+
+    res.send();
+}, (error, req, res, next) => {
+    // upper declaration is needed for the middleware to properly handle thrown errors
+    res.status(400).send(error.message);
+})
+
+
+userRouter.delete('/users/profile/avatar', auth, async (req, res) => {
+    const user = req.user;
+
+    try {
+        if (!user.avatar) {
+            return res.send('No avatar provided');
+        }
+
+        user.avatar = undefined;
+        user.save()
+        res.send('successfully deleted user avatar');
+
+    } catch (err) {
+        res.status(500).send(err)
+    }
+});
+
+
+
+userRouter.get('/users/:id/avatar', async (req, res) => {
+    const _id = req.params.id;
+    const user = await User.findOne({ _id });
+
+    try {
+        if (!user || !user.avatar) {
+            return res.send('No avatar provided');
+        }
+
+        res.set('Content-Type', 'image/jpg')
+        res.send(user.avatar);
+
+    } catch (err) {
+        res.status(500).send(err)
+    }
+});
 
 userRouter.post('/users', async (req, res) => {
     try {
@@ -14,11 +78,6 @@ userRouter.post('/users', async (req, res) => {
     } catch (e) {
         res.status(404).send(e)
     };
-
-    // user.save()
-    //     .then((resonse) => res.status(201).send(user))
-    //     .catch(err => res.status(404).send(err));
-
 })
 
 
@@ -49,7 +108,6 @@ userRouter.patch('/users/profile', auth, async (req, res) => {
     }
 
     try {
-        // const user = await User.findById(req.user._id);
         const { user } = req;
         requestedUpdates.forEach(update => user[update] = req.body[update]);
 
@@ -64,9 +122,7 @@ userRouter.patch('/users/profile', auth, async (req, res) => {
 
 userRouter.delete('/users/profile', auth, async (req, res) => {
 
-    console.log('req.user', req.user);
     try {
-
         await req.user.remove();
         console.log('users/profile delete', req.user);
 
@@ -112,9 +168,5 @@ userRouter.post('/users/logoutAll', auth, async (req, res) => {
         return res.status(500).send('Could not logout from all devices.');
     }
 })
-
-
-
-
 
 module.exports = userRouter;
